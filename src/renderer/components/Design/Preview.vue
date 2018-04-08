1f9db1079524b4f2f6ce5c043e38c15f7ecc6fe3
Griffin Wiebel <griffinwiebel@gmail.com>
2018-04-08 01:04:31 -0700

    Initial repo transfer Transferring all controller based code into a seperate repo

---
+++ src/renderer/components/Design/Preview.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="preview">
+    <h3>Preview</h3>
+    <p>Back</p>
+    <stage-renderer></stage-renderer>
+    <p>Front</p>
+  </section>
+</template>
+
+<script>
+import StageRenderer from './StageRenderer.vue';
+export default {
+  components: {
+    StageRenderer
+  }
+};
+</script>
+
+<style scoped>
+.preview {
+    height: 100%;
+}
+p {
+  font-size: 12px;
+  text-align: center;
+}
+</style>
+
---
+++ src/renderer/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <preview></preview>
+    <testing></testing>
+  </div>
+</template>
+
+<script>
+import Preview from './Design/Preview';
+import Testing from './Test/Testing';
+export default {
+  components: {
+    Preview,
+    Testing
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
